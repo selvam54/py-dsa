51ce0400a1e88f422a429b40f84ac8783de4d958
selvam54 <s.selvam00000001@gmail.com>
2025-11-16 19:11:28 +0530

    add new

---
+++ part2/array6.py
@@ -0,0 +1,10 @@
+#Rotate array
+import random
+
+array=[2,4,2,3,5,33,44,66,55]
+print(array)
+rotate_array=[]
+
+random.shuffle(array)
+
+print(array)
---
+++ part2/array7.py
@@ -0,0 +1,6 @@
+# Find missing number
+
+array=list(map(int,input("enter the array : ").split()))
+
+print("the first array :",array)
+
